609b4619a28560091841032bb9e2509b7237bb2e
itsmorais <michael_morais@outlook.com.br>
2022-11-30 21:50:44 -0300

    homepage done

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Details} from './pages/Details/index'
-import {ThemeProvider} from 'styled-components'
+import { Home } from './pages/Home/index'
+import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <GlobalStyles/>
-    <Details />
-    </ThemeProvider>  
+      <GlobalStyles />
+      <Home />
+    </ThemeProvider>
   </React.StrictMode>
-  
+
+
+
+
 )
