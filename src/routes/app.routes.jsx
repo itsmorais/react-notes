3ac4c13720bfe48ccbb1347cf1c4f03df345eaf7
itsmorais <michael_morais@outlook.com.br>
2022-12-21 13:20:16 -0300

    Home bug fixed

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 
-import { New } from '../pages/New';
 import { Home } from '../pages/Home';
+import { New } from '../pages/New';
 import { Detail } from '../pages/Detail';
 import { Profile } from '../pages/Profile';
 
