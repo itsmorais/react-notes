c5ea605829d5578f7e4660ef63b72d68476d59d5
itsmorais <michael_morais@outlook.com.br>
2022-12-21 12:36:37 -0300

    folder Case-names

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 
 import { New } from '../pages/New';
 import { Home } from '../pages/Home';
--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,7 +1,7 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 
-import { SignUp } from '../pages/SignUp'
-import { SignIn } from '../pages/SignIn'
+import { SignUp } from '../pages/SignUp';
+import { SignIn } from '../pages/SignIn';
 
 
 export function AuthRoutes() {
